0d1abdccd9a1ec135f69f5e47a2c09b703dac9f9
Lsong <song940@gmail.com>
2021-10-21 19:37:15 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const fs = require('fs');
 const {
   parseStackTrace,
   printStackTrace
 } = require('superror');
 const highlight = require('./highlight');
+const { readFile } = require('fs').promises;
+
+const call = p =>
+  p.then(r => [null, r], e => [e]);
+
+const { NODE_ENV = 'development' } = process.env;
 
 module.exports = async (req, res, next) => {
-  try {
-    await next();
-  } catch (err) {
+  const year = new Date().getFullYear();
+  const handleError = async err => {
     if (err instanceof Error) {
       const [stack] = parseStackTrace(err.stack);
       const { location, line, column } = stack;
       console.error(err.name, err.message, `at ${location}:${line}:${column}`);
-      fs.readFile(location, 'utf8', (err, src) => {
-        if (err) return err;
-        console.error(highlight(printStackTrace(src, line, column)));
-      });
+      const [ferr, code] = await call(readFile(location, 'utf8'));
+      if (!ferr) {
+        const output = printStackTrace(code, line, column);
+        console.error(highlight(output));
+      }
     }
     if (res.statusCode / 100 | 0 != 5) {
       res.statusCode = 500;
@@ -25,9 +30,15 @@ module.exports = async (req, res, next) => {
       'Content-Type': 'text/html'
     });
     res.write('<h2>Server Internal Error</h2>');
-    res.write(`<pre>${err.stack}</pre>`);
+    if (NODE_ENV === 'development')
+      res.write(`<pre>${err.stack}</pre>`);
     res.write('<hr />');
-    res.write(`Powered by <a href="https://github.com/song940/kelp-error" >kelp-error</a> &copy; ` + new Date().getFullYear());
+    res.write(`Powered by <a href="https://github.com/kelpjs/kelp-error" >kelp-error</a> &copy; ` + year);
     res.end();
+  };
+  try {
+    await next();
+  } catch (err) {
+    handleError(err);
   }
 }
