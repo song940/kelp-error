91807aaad036f3ec82530580b06494c2aa3bea7e
Lsong <song940@gmail.com>
2021-10-21 19:40:04 +0800

    update

--- index.js
+++ index.js
@@ -10,35 +10,36 @@ const call = p =>
 
 const { NODE_ENV = 'development' } = process.env;
 
-module.exports = async (req, res, next) => {
+const handleError = async (err, res) => {
   const year = new Date().getFullYear();
-  const handleError = async err => {
-    if (err instanceof Error) {
-      const [stack] = parseStackTrace(err.stack);
-      const { location, line, column } = stack;
-      console.error(err.name, err.message, `at ${location}:${line}:${column}`);
-      const [ferr, code] = await call(readFile(location, 'utf8'));
-      if (!ferr) {
-        const output = printStackTrace(code, line, column);
-        console.error(highlight(output));
-      }
+  if (err instanceof Error) {
+    const [stack] = parseStackTrace(err.stack);
+    const { location, line, column } = stack;
+    console.error(err.name, err.message, `at ${location}:${line}:${column}`);
+    const [ferr, code] = await call(readFile(location, 'utf8'));
+    if (!ferr) {
+      const output = printStackTrace(code, line, column);
+      console.error(highlight(output));
     }
-    if (res.statusCode / 100 | 0 != 5) {
-      res.statusCode = 500;
-    }
-    res.writeHead(res.statusCode, {
-      'Content-Type': 'text/html'
-    });
-    res.write('<h2>Server Internal Error</h2>');
-    if (NODE_ENV === 'development')
-      res.write(`<pre>${err.stack}</pre>`);
-    res.write('<hr />');
-    res.write(`Powered by <a href="https://github.com/kelpjs/kelp-error" >kelp-error</a> &copy; ` + year);
-    res.end();
-  };
+  }
+  if (res.statusCode / 100 | 0 != 5) {
+    res.statusCode = 500;
+  }
+  res.writeHead(res.statusCode, {
+    'Content-Type': 'text/html'
+  });
+  res.write('<h2>Server Internal Error</h2>');
+  if (NODE_ENV === 'development')
+    res.write(`<pre>${err.stack}</pre>`);
+  res.write('<hr />');
+  res.write(`Powered by <a href="https://github.com/kelpjs/kelp-error" >kelp-error</a> &copy; ` + year);
+  res.end();
+};
+
+module.exports = async (req, res, next) => {
   try {
     await next();
   } catch (err) {
-    handleError(err);
+    handleError(err, res);
   }
 }
